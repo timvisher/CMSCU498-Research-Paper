bdf736479ac590cfc5714bbca8117a486492dea8
Tim Visher <tim.visher@gmail.com>
2010-04-08 02:07:39 -0400

    Reference the Groovy file directly. EOM

--- randomSourceGen.groovy
+++ randomSourceGen.groovy
@@ -1,6 +1,6 @@
-file = new File('/Users/tim/Documents/CMSCU 498/Research Paper/'+args[0]+'.csv')
+file = new File(args[0] + '.csv')
 
-while (950 > file.length() / 1000) {
+while (10000 >= file.size()) {
     def random = Math.random()
     if (0.5 >= Math.random()) {
         random *= -1
