4e463b6e2efca6d97df5360886bbbf01f9b1aaf6
Tim Visher <tim.visher@gmail.com>
2010-04-09 03:44:23 -0400

    Restablished build process and stitched together final research paper. EOM

---
+++ build.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d build ] ; then
+    rm -r build
+fi
+
+TEX_FILE=researchPaper
+
+#setup build structure
+mkdir build
+mkdir build/latex
+
+#move required files in
+find . -name '*.tex'  -exec cp '{}' build/latex/ \;
+find . -name '*.bib' -exec cp '{}' build/latex/ \;
+find . -name '*.groovy' -exec cp '{}' build/latex/ \;
+
+#LaTeX
+cd build/latex
+latex $TEX_FILE.tex
+latex $TEX_FILE.tex
+bibtex $TEX_FILE.aux
+latex $TEX_FILE.tex
+pdflatex $TEX_FILE.tex
+mv $TEX_FILE.pdf ../
+
